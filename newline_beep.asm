23c67bb67055df79b881fd4066979133994d6117
MMunim <mmunim9.9.01@gmail.com>
2025-10-10 23:56:50 +0600

    explore beep sound and new line

---
+++ newline_beep.asm
@@ -0,0 +1,70 @@
+.model small
+.stack 100h
+
+.data
+a db "Coded by Munim $"
+
+.code 
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    mov dx,offset a
+    int 21h
+    
+    mov al,'m'
+    
+    mov ah,2
+    mov dl,al
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    mov ah,1
+    int 21h
+    mov bh,al
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    mov ah,2
+    mov dl,bh
+    int 21h
+    
+    mov ah,2
+    mov dl,07
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
