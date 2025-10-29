a41356b63279d7f38e858872a705c1ba73a8cbb7
MMunim <mmunim9.9.01@gmail.com>
2025-10-29 20:56:49 +0600

    multiplication in assembly

---
+++ multiplication.asm
@@ -0,0 +1,53 @@
+; mul -> unsigned
+; imul -> signed/integer
+; syntax mul/imul source
+
+.model small
+.stack 100h
+.data
+a db ?
+b db ?
+c db 'Input First Digit: $'
+d db 10,13,'Input Second Digit: $'
+e db 10,13,'Result: $'
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,c
+    int 21h
+    
+    mov ah,1
+    int 21h
+    sub al,48
+    mov a,al
+    
+    mov ah,9
+    lea dx,d
+    int 21h
+    
+    mov ah,1
+    int 21h
+    sub al,48
+    mov b,al
+    
+    mov ah,9
+    lea dx,e
+    int 21h
+    
+    mov al,a
+    mov bl,b  ;a x b = ax 
+    mul bl
+    
+    add al,48
+    mov dl,al
+    mov ah,2
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
